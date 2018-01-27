680bc76cd21a7ea7876312a1aebd09c40b97b00d
Cedric Faché <cedric.fache@hotmail.com>
2018-01-27 13:50:04 +0100

    Changed server port to 80.

--- project.py
+++ project.py
@@ -821,4 +821,4 @@ def getUser(email):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
