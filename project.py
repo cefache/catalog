c62011cbffde77a38a1c6a7732bbeb918bb78c1b
Cedric Faché <cedric.fache@hotmail.com>
2018-01-28 15:57:56 +0100

    Make project file up to date with postgresql changes

--- project.py
+++ project.py
@@ -25,7 +25,7 @@ CLIENT_ID = json.loads(open('client_secrets.json',
 APPLICATION_NAME = "catalog-application"
 
 # Connect to Database and create database session
-engine = create_engine('postgresql:///catalog.db')
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
