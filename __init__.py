94b84e76edfc93663b59c406a4c0bde83644838c
Cedric Faché <cedric.fache@hotmail.com>
2018-02-01 07:42:49 +0100

    changes in secret key

--- __init__.py
+++ __init__.py
@@ -817,9 +817,9 @@ def getUser(email):
     users = session.query(User).filter_by(email=email).first()
     return jsonify(users.serialize)
 
-
+app.secret_key = 'super_secret_key'
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+    #app.secret_key = 'super_secret_key'
     app.debug = True
     # app.run(host='0.0.0.0', port=80)
     app.run()
